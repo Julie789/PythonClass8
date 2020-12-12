0c727a942a954486cedd36afd2800fa53780e0ff
Julie Sunny Mathew <42555719+Julie789@users.noreply.github.com>
2020-12-12 15:23:59 -0600

    Update EncryptionTools.py

--- EncryptionTools.py
+++ EncryptionTools.py
@@ -5,25 +5,11 @@ import Enigma
 
 def main():
     #Caesar Cipher
-    message = input("Enter a message: ")
-
-    key = int(input("Enter a key: "))
-    secret = CaesarCipher.encode(message, key)
-    print ("Caesar Encrypted:", secret)
 
     #Vigenère Cipher
-    #keyword = input("Enter keyword: ")
-    #secret = VigenereCipher.encode(message, keyword)
-    #print ("Vigenere Encrypted:", secret)
 
     #Substitution Cipher
-    #key = input("Enter a key: ")
-    #secret = SubstitutionCipher.encode(message, key)
-    #print ("Substitution Encrypted:", secret)
 
     #Enigma Cipher
-    #Enigma.setStart("A", "B", "C")
-    #secret = Enigma.encode(message)
-    #print ("Enigma Encrypted:", secret)
 
 main()
